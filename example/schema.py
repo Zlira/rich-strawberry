e7f90608b4275f7e098c1855f5cd2a92a99ba819
zlira <olesia.hr@gmail.com>
2023-01-31 20:24:00 +0200

    An example for logger as an extension

--- example/schema.py
+++ example/schema.py
@@ -1,6 +1,28 @@
+import logging
 import strawberry
+from strawberry.extensions import Extension
 
 from rich_strawberry import RichGraphQLLogger, SchemaWithRichLogger
+
+logger = logging.getLogger("strawberry.execution")
+logger.disabled = True
+debug_logger = RichGraphQLLogger(
+    log_context_keys=[
+        "request",
+    ]
+)
+
+
+class RichLoggerExtension(Extension):
+    def __init__(self, logger: RichGraphQLLogger):
+        self.debug_logger = logger
+
+    def on_request_end(self):
+        if self.execution_context.errors:
+            self.debug_logger.print_errors(
+                self.execution_context.errors,
+                self.execution_context,
+            )
 
 
 @strawberry.type
@@ -11,5 +33,7 @@ class Query:
         return 139
 
 
-debug_logger = RichGraphQLLogger(suppress_traceback_from=[])
-schema = SchemaWithRichLogger(query=Query, debug_logger=debug_logger)
+schema = SchemaWithRichLogger(
+    query=Query,
+    extensions=[RichLoggerExtension(debug_logger)],
+)
